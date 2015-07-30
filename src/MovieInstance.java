cfe77d3177a295c1106d58f1602f186901cde70e
Matt Kormann <matt.kormann@gmail.com>
2015-07-29 22:06:29 -0400

    Added logic to MovieInstance

--- src/MovieInstance.java
+++ src/MovieInstance.java
@@ -8,14 +8,44 @@
 public class MovieInstance {
 
 	private final Movie movie;
-	private Long startTime;
+	private int startTime = 0;
+	private boolean started;
+	private boolean finished;
 	
 	public MovieInstance(Movie movie) {
 		this.movie = movie;
 	}
 	
-	public void setStartTime() {
-		if (startTime != null) return;
-		else startTime = System.nanoTime() / 1000000000;
+	/**
+	 * Sets movie start time with current System.nanoTime()
+	 */
+	public void startMovie() {
+		if (!started) {
+			startTime = (int)System.nanoTime() / 1000000000;
+			started = true;
+		}
+	}
+	
+	/**
+	 * Returns current time of movie in seconds, returning 0 if not started, and -1 if finished
+	 * @return int representing the current play time in seconds
+	 */
+	public int getCurrentTime() {
+		if (!started) return 0;
+		if (!finished) return (int)(System.nanoTime() / 1000000000 - startTime);
+		else return -1;
+	}
+	
+	/**
+	 * Checks if movie is done playing
+	 * @return true if done, false otherwise
+	 */
+	public boolean isOver() {
+		if (finished) return true;
+		if (System.nanoTime() / 1000000000 - startTime > movie.getLength()) {
+			finished = true;
+			return true;
+		}
+		return false;
 	}
 }
